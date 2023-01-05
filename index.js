7785aa9f05fe231be21600d5aace0d3a486b040e
luqmanshaban <luqmanforeal@gmail.com>
2023-01-05 13:45:02 +0300

    coding the body and making it responsive

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
+
+document.querySelectorAll('.navLink').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}))
